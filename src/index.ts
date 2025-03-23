98fe0a0e5475fbcfb2b11adc61eccd7b27ad0c33
Alexander Ilseman <alexander.ilseman.com>
2025-03-23 05:49:42 -0500

    Using svg api to draw boid triangles

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
 import "./styles.css";
 
-(function () {
-  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-  const ctx = canvas.getContext("2d");
+function point(ctx: CanvasRenderingContext2D, rads: number): void {
+  ctx.translate(15, 10);
+  ctx.rotate(rads);
+  ctx.translate(-15, -10);
+}
 
-  ctx.beginPath();
-  ctx.moveTo(50, 50);
-  ctx.lineTo(65, 45);
-  ctx.lineTo(65, 55);
-  ctx.fill();
-})();
+interface Boid {
+  xPos: number;
+  yPos: number;
+  heading: number;
+  speed: number;
+}
+
+function drawBoid(boid: Boid): Path2D {
+  // TODO: Create a function that translates a boid's "position" to a path value
+  // and also convert it's heading to the appropriate rotation
+  const p = new Path2D("M0 10 L30 0 L25 10 L30 20Z");
+}
+
+// TODO: Create a function for incrimenting a boid's position based on it's velocity
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+point(ctx, (Math.PI * 3) / 2);
+// Translate in the direction the boid is pointing
+const dy = Math.sin(theta);
+
+ctx.fill(p);
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,48 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
   plugins: [
     new HtmlWebpackPlugin({
       hash: true,
-      title: "Boid Flocking Sim",
-      header: "Flocky Boids!",
-      metaDesc: "Boid Flocking Sim",
-      template: "src/index.html",
-      filename: "index.html",
-      inject: "body",
+      title: 'Boid Flocking Sim',
+      header: 'Flocky Boids!',
+      metaDesc: 'Boid Flocking Sim',
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body',
     }),
   ],
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/inline',
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
   output: {
     clean: true,
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, 'dist'),
     },
     open: true,
     hot: true,
